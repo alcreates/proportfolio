54b7e098fa6e44610379bdc3cf49e381d941366b
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2016-06-12 15:03:44 -0400

    node mailer test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express 	= require('express');
 var bodyParser 	= require('body-parser');
+var nodemailer = require('nodemailer');
 
 
 
@@ -17,6 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
+var transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com');
 
 
 
@@ -26,6 +28,30 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 app.get('/', function(req,res){
 	res.send(index.html);
 });
+
+app.post('/sendEmail', function(req, res){
+
+
+	console.log(req);
+
+// 	var mailOptions = {
+//     from: '"Fred Foo 👥" <foo@blurdybloop.com>', // sender address 
+//     to: 'bar@blurdybloop.com, baz@blurdybloop.com', // list of receivers 
+//     subject: 'Hello ✔', // Subject line 
+//     text: 'Hello world 🐴', // plaintext body 
+//     html: '<b>Hello world 🐴</b>' // html body 
+// };
+ 
+// // send mail with defined transport object 
+// transporter.sendMail(mailOptions, function(error, info){
+//     if(error){
+//         return console.log(error);
+//     }
+//     console.log('Message sent: ' + info.response);
+// });
+
+})
+
 
 // Starts the server to begin listening 
 // =============================================================
