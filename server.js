c5fe043cd95e9aefd964f87fefe6dd7b4c77b517
Alvaro Martinez <alvaromartinez@Alvaros-MacBook-Pro-2.local>
2016-08-23 10:51:09 -0400

    update resume

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ var mailOpts, smtpTrans;
 });
 
 app.get('/download', function(req, res){
-  var file = __dirname + '/public/assets/downloads/alvaroResume0724.pdf';
+  var file = __dirname + '/public/assets/downloads/alvaroResume0820.pdf';
   res.download(file); // Set disposition and send it.
 });
 
